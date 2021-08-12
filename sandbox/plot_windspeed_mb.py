9ab003973057ae806fe0ee0cb85be25c487e241e
Lori Garzio <lgarzio@marine.rutgers.edu>
2021-08-12 15:08:20 -0400

    plot windspeed at 925mb

---
+++ sandbox/plot_windspeed_mb.py
@@ -0,0 +1,4 @@
+#def main(wrf_procdir, save_dir):
+    # wrf_dir = '/Users/garzio/Documents/rucool/bpu/wrf/website_plots_redo/processed/seabreeze/20200729'
+    # sDir = '/Users/garzio/Documents/rucool/bpu/wrf/website_plots_redo/webfigs/3km_seabreeze'
+    # main(wrf_dir, sDir)
