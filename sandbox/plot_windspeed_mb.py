5b3cb20262191890b686fa6ffe21a1749e4741d0
Lori Garzio <lgarzio@marine.rutgers.edu>
2021-08-20 14:14:21 -0400

    option to add windspeed text to maps

--- sandbox/plot_windspeed_mb.py
+++ sandbox/plot_windspeed_mb.py
@@ -24,7 +24,7 @@ import functions.plotting as pf
 plt.rcParams.update({'font.size': 12})  # all font sizes are 12 unless otherwise specified
 
 
-def plt_windsp(nc, model, ht, figname, lease_areas=None, summary=None):
+def plt_windsp(nc, model, ht, figname, lease_areas=None, summary=None, add_text=None):
     """
     Create pseudocolor surface maps of wind speed with quivers indicating wind direction.
     :param nc: netcdf file
@@ -34,10 +34,11 @@ def plt_windsp(nc, model, ht, figname, lease_areas=None, summary=None):
     :param lease_areas: optional dictionary containing lat/lon coordinates for wind energy lease area polygon
     :param summary: optional dictionary containing locations of specific locations for seabreeze classification,
     and a list to append windspeeds at specific locations for summary output
-
+    :param add_text: optional, add windspeed/direction values at specific locations to the figure
     """
     lease_areas = lease_areas or None
     summary = summary or None
+    add_text = add_text or None
 
     u = nc.sel(pressure=ht)['UP']
     v = nc.sel(pressure=ht)['VP']
@@ -88,15 +89,20 @@ def plt_windsp(nc, model, ht, figname, lease_areas=None, summary=None):
         direction = cf.wind_uv_to_dir(u_sub, v_sub)
 
         # write a summary for wind speeds/directions at specified locations for seabreeze classification
+        # add the windspeeds/directions to the map
+        map_values = dict()
         if summary:
-            if pt == 'full_grid':
-                tmstr = pd.to_datetime(nc.Time.values[0]).strftime('%Y-%m-%dT%H:%M')
-                for key, coords in summary['locations'].items():
-                    # find the closest model grid point to the location
-                    a = abs(speed.XLAT - coords['lat']) + abs(speed.XLONG - coords['lon'])
-                    i, j = np.unravel_index(a.argmin(), a.shape)
-                    sp = speed[i, j]
-                    d = direction[i, j]
+            tmstr = pd.to_datetime(nc.Time.values[0]).strftime('%Y-%m-%dT%H:%M')
+            for key, coords in summary['locations'].items():
+                # find the closest model grid point to the location
+                a = abs(speed.XLAT - coords['lat']) + abs(speed.XLONG - coords['lon'])
+                i, j = np.unravel_index(a.argmin(), a.shape)
+                sp = speed[i, j]
+                d = direction[i, j]
+                coords.update(ws=np.round(float(sp.values), 2))
+                coords.update(direction=np.round(float(d.values), 2))
+                map_values[key] = coords
+                if pt == 'full_grid':
                     wrf_lat = np.round(float(sp.XLAT.values), 4)
                     wrf_lon = np.round(float(sp.XLONG.values), 4)
                     summary['rows'].append([tmstr, key, coords['lat'], coords['lon'], ht, wrf_lat, wrf_lon,
@@ -150,8 +156,16 @@ def plt_windsp(nc, model, ht, figname, lease_areas=None, summary=None):
 
         # add the seabreeze classification locations to the map
         if summary:
-            for key, coords in summary['locations'].items():
-                ax.scatter(coords['lon'], coords['lat'], c='magenta', s=40, zorder=15, transform=ccrs.PlateCarree())
+            if pt == 'full_grid':
+                offset = 1.75
+            else:
+                offset = .85
+            for key, values in map_values.items():
+                ax.scatter(values['lon'], values['lat'], c='magenta', s=40, zorder=15, transform=ccrs.PlateCarree())
+                if add_text:
+                    ax.text(values['lon'] - offset, values['lat'], '{} {}'.format(values['ws'], values['direction']),
+                            transform=ccrs.PlateCarree(),
+                            bbox=dict(facecolor='lightgray', alpha=1), fontsize=8, zorder=15)
 
         plt.savefig(figname, dpi=200)
         plt.close()
@@ -188,6 +202,7 @@ def main(args):
     # kwargs['lease_areas'] = cf.extract_lease_areas()
     summary = dict(rows=[], locations=seabreeze_locations)
     kwargs['summary'] = summary
+    kwargs['add_text'] = True
 
     for i, f in enumerate(files):
         fname = f.split('/')[-1].split('.')[0]
