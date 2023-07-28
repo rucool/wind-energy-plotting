b9b7fc1cd1ee189f2b56c1a3f93d144613dae8f8
JamesKimMET <116097439+JamesKimMET@users.noreply.github.com>
2023-07-28 10:49:22 -0400

    Update plot_sst_sport_test_final.py

--- wrf_surface_maps/plot_sst_sport_test_final.py
+++ wrf_surface_maps/plot_sst_sport_test_final.py
@@ -65,13 +65,13 @@ def main(args):
         SST_WRF_file = glob.glob(os.path.join(sst_inputs_dir, f'SST_WRF_{ymd}.grb'))[0]
     except IndexError:
         print(f'No such file or directory: SST_WRF_{ymd}.grb')
-        sys.exit(1)
+        
 
     try:
         sport_avhrr_file = glob.glob(os.path.join(sport_avhrr_dir, f'procdate_{sport_dd}*.nc'))[0]
     except IndexError:
         print(f'No such file or directory: procdate_{sport_dd}*.nc')
-        sys.exit(1)
+        
 
     save_dir_zoom_out = os.path.join(save_dir, f'{str(yr)}_sport', 'zoom_out')
     save_dir_zoom_in = os.path.join(save_dir, f'{str(yr)}_sport', 'zoom_in')
