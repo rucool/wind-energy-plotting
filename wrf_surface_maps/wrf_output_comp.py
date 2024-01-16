e69b48f1706783bca76932df7b72d9f2568b02f4
JamesKimMET <116097439+JamesKimMET@users.noreply.github.com>
2024-01-16 11:38:12 -0500

    Create wrf_output_comp.py
    added new script to plot ru-wrf sst OUTPUT from 1km runs

---
+++ wrf_surface_maps/wrf_output_comp.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+
+"""
+Author: [Your Name]
+Last modified: [Modification Date]
+Description: Creates surface maps of sea surface temperature (SST) from RU-WRF 4.1 output.
+"""
+
+import argparse
+import numpy as np
+import pandas as pd
+import os
+import glob
+import sys
+import xarray as xr
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+from matplotlib.colors import BoundaryNorm
+import cartopy.crs as ccrs
+import cmocean as cmo
+import functions.common as cf
+import functions.plotting as pf
+
+plt.rcParams.update({'font.size': 12})  # all font sizes are 12 unless otherwise specified
+
+
+def subset_grid(ext, dataset, lon_name, lat_name):
+    """
+    Subsets the grid based on the provided geographical extent.
+
+    Args:
+        ext (list): Geographical extent [lon_min, lon_max, lat_min, lat_max].
+        dataset (xarray.Dataset): The dataset containing SST data.
+        lon_name (str): Name of the longitude variable in the dataset.
+        lat_name (str): Name of the latitude variable in the dataset.
+
+    Returns:
+        tuple: Subsets of the dataset and corresponding longitude and latitude.
+    """
+    if len(np.shape(dataset[lon_name])) == 1:
+        lonx, laty = np.meshgrid(dataset[lon_name], dataset[lat_name])
+    else:
+        lonx = dataset[lon_name]
+        laty = dataset[lat_name]
+
+    lon_idx = np.logical_and(lonx > ext[0], lonx < ext[1])
+    lat_idx = np.logical_and(laty > ext[2], laty < ext[3])
+
+    ind = np.where(np.logical_and(lat_idx, lon_idx))
+    data_sub = np.squeeze(dataset)[range(np.min(ind[0]), np.max(ind[0]) + 1), range(np.min(ind[1]), np.max(ind[1]) + 1)]
+    lon = data_sub[lon_name]
+    lat = data_sub[lat_name]
+
+    return data_sub, lon, lat
+
+
+def main(args):
+    ymd = args.ymd
+    save_dir = args.save_dir
+
+    yr = pd.to_datetime(ymd).year
+    ym = ymd[0:6]
+
+    save_dir_wrf = os.path.join(save_dir, str(yr), 'wrf_output', ym)
+    os.makedirs(save_dir_wrf, exist_ok=True)
+
+    wrf_dir = os.path.join('/home/coolgroup/ru-wrf/real-time/v4.1_parallel/processed/1km_wf2km_nyb', ymd)  # Update the path as needed
+
+    extent = [-80, -60, 31, 46]  # Define the geographical extent as needed
+
+    # get the WRF output SST
+    wrf_file = glob.glob(os.path.join(wrf_dir, 'wrfproc_*_00Z_H000.nc'))[0]
+    ds = xr.open_dataset(wrf_file)
+    sst_wrf = np.squeeze(ds.SST) - 273.15  # convert K to degrees C
+
+    # Setup plot
+    fig, ax = plt.subplots(figsize=(9, 8), subplot_kw=dict(projection=ccrs.Mercator()))
+    main_title = f'RU-WRF Sea Surface Temperature: {pd.to_datetime(ymd).strftime("%Y-%m-%d")}'
+    fig.suptitle(main_title, fontsize=16, y=.98)
+
+    # Plotting
+    vlims = [5, 30]
+    cmap = cmo.cm.thermal
+    levels = MaxNLocator(nbins=16).tick_values(vlims[0], vlims[1])
+    norm = BoundaryNorm(levels, ncolors=cmap.N, clip=True)
+
+    sst_wrf_sub, lon_wrf, lat_wrf = subset_grid(extent, sst_wrf, 'XLONG', 'XLAT')
+    pf.plot_pcolormesh_panel(fig, ax, lon_wrf, lat_wrf, sst_wrf_sub.values, 
+                             panel_title='RU-WRF Output', 
+                             norm_clevs=norm, 
+                             extend='both', 
+                             cmap=cmap, 
+                             clab='SST (\N{DEGREE SIGN}C)')
+
+    plt.savefig(os.path.join(save_dir_wrf, f'ru-wrf_sst_{ymd}.png'), dpi=200)
+    plt.close()
+
+if __name__ == '__main__':
+    arg_parser = argparse.ArgumentParser(description='Plot WRF SST output',
+                                         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    arg_parser.add_argument('ymd',
+                            type=str,
+                            help='Year-month-day to plot in the format YYYYmmdd (e.g. 20220101.')
+
+    arg_parser.add_argument('-save_dir',
+                            default='/www/web/rucool/windenergy/ru-wrf/images/upwelling_case/upwelling_sst_output',  # Update the path as needed
+                            type=str,
+                            help='Full directory path to save output plots.')
+
+    parsed_args = arg_parser.parse_args()
+    sys.exit(main(parsed_args))
